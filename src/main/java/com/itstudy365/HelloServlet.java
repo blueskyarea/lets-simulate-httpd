aede819145cbc7a2c2789ac3734edb9d8e4f3df9
blueskyarea <blueskyareahm@gmail.com>
2025-08-07 23:31:27 +0900

    Delete filebeat.

--- src/main/java/com/itstudy365/HelloServlet.java
+++ src/main/java/com/itstudy365/HelloServlet.java
@@ -2,6 +2,9 @@ package com.itstudy365;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,10 +14,12 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    protected final static Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        logger.info("Accessed to /hello the application.");
         response.getWriter().println("Hello from Tomcat!");
     }
 }
